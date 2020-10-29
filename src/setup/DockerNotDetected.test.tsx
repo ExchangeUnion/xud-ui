74e63a90183a7e605a843e185ee2e1e5557e14fd
Karl Ranna <karl@karlranna.com>
2020-10-29 11:41:39 -0700

    feat: show boilerplate landing page for windows
    This commit adds boilerplate `Landing` screen. This screen is currently
    only being shown to Windows users.

--- src/setup/DockerNotDetected.test.tsx
+++ src/setup/DockerNotDetected.test.tsx
@@ -6,6 +6,7 @@ test("renders 'Waiting for XUD Docker' text when promise is pending", () => {
   window.electron = {
     send: () => {},
     receive: () => {},
+    platform: () => "linux",
   };
   const { getByText } = render(<App />);
   const headerTextElement = getByText(/Waiting for XUD Docker/i);
