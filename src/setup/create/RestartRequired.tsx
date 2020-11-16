2aee6992100497427e82ba1467ac0d549d3e0cfa
krr prr <24500376+krrprr@users.noreply.github.com>
2020-11-16 13:08:15 +0200

    feat: change reboot button's text 'Reboot Now' -> 'Reboot PC'

--- src/setup/create/RestartRequired.tsx
+++ src/setup/create/RestartRequired.tsx
@@ -65,7 +65,7 @@ const RestartRequired = (): ReactElement => {
               });
             }}
           >
-            Reboot Now
+            Reboot PC
           </Button>
         </Grid>
         <LinkToDiscord />
