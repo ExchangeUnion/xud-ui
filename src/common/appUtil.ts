5c5e0b645eec67be8593594ebf4bcf80c2975436
krr prr <24500376+krrprr@users.noreply.github.com>
2020-12-17 18:49:36 +0200

    fix: prevent empty screen instead of dashboard; log to file instead of console

--- src/common/appUtil.ts
+++ src/common/appUtil.ts
@@ -1,3 +1,11 @@
 export const isWindows = (): boolean => {
   return (window as any).electron.platform() === "win32";
 };
+
+export const logError = (...message: any[]): void => {
+  (window as any).electron.logError(message.join("\n"));
+};
+
+export const logInfo = (...message: any[]): void => {
+  (window as any).electron.logInfo(message.join("\n"));
+};
